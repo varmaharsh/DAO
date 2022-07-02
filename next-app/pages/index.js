3bb17203eb2077306deed5aea79e2f0074fc6059
varmaharsh <varmaharsh15@gmail.com>
2022-07-02 13:45:18 +0530

    changes package lock and footer

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -376,7 +376,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made with &#10084; by Harsh Varma
       </footer>
     </div>
   );
